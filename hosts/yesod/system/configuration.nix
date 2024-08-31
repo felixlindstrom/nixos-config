54d25e86dbf78dbfc6deb8989c31545acf93f9b5
Felix Lindström <felix.lindstrom@gmail.com>
2024-08-31 10:13:14 +0200

    Enable fans and camere on Yesod.

--- hosts/yesod/system/configuration.nix
+++ hosts/yesod/system/configuration.nix
@@ -4,6 +4,12 @@
     ./modules
   ];
 
+  environment.systemPackages = [
+    pkgs.mbpfan
+  ];
+
+  services.mbpfan.enable = true;
+
   networking.hostName = "yesod";
 
   system.stateVersion = "24.05";
