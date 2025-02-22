769381228a4c629404b22f8800e7d2e2b3cae685
Felix Lindström <felix.lindstrom@gmail.com>
2025-02-22 20:50:51 +0100

    Minor cleanup

--- hosts/_shared/home/modules/config.nix
+++ hosts/_shared/home/modules/config.nix
@@ -1,25 +1,16 @@
-{ config, ... }:
+{ ... }:
 let
   configDir = ../../../../config;
   binDir = ../../../../bin;
   assetsDir = ../../../../assets;
-
-  mkooss = config.lib.file.mkOutOfStoreSymlink;
-  linkConfig = filePath: mkooss "${config.users.users.felix.home}/Projects/nixos-config/config/${filePath}"; # Change this to be relative..
 in
 {
   xdg.configFile."hypr" = {
     source = "${configDir}/hypr";
     recursive = true;
   };
-  #xdg.configFile."waybar".source = linkConfig "waybar";
-  # xdg.configFile.rofi = {
-  #   source = config.lib.file.mkOutOfStoreSymlink "${configDir}/rofi";
-  #   recursive = true;
-  # };
   home.file = {
-    # ".config/hypr".source = "${configDir}/hypr";
-    ".config/sway".source = "${configDir}/sway";
+    ".config/mako".source = "${configDir}/mako";
     ".config/rofi".source = "${configDir}/rofi";
     ".config/waybar".source = "${configDir}/waybar";
     ".config/wezterm".source = "${configDir}/wezterm";
