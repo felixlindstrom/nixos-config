f908111dd2ce0800979d88cec6581b6489e869c9
Felix Lindström <felix.lindstrom@gmail.com>
2025-03-27 21:21:12 +0100

    Update flake.

--- config/nvim/lua/plugins/core/snacks.lua
+++ config/nvim/lua/plugins/core/snacks.lua
@@ -8,7 +8,7 @@ return {
     input = { enabled = false },
     notifier = { enabled = false, timeout = 4000 },
     quickfile = { enabled = true },
-    words = { enabled = true },
+    words = { enabled = false },
     scratch = {
       enabled = false,
     },
--- config/nvim/lua/plugins/ui/colorscheme.lua
+++ config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "Mofiqul/vscode.nvim",
-    config = function()
-      require("vscode").setup()
-    end,
-  },
-
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    opts = {
-      priority = 1000,
-    },
-    config = function()
-      require("github-theme").setup({})
-    end,
-  },
+  -- {
+  --   "Mofiqul/vscode.nvim",
+  --   config = function()
+  --     require("vscode").setup()
+  --   end,
+  -- },
+  --
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   lazy = false,
+  --   opts = {
+  --     priority = 1000,
+  --   },
+  --   config = function()
+  --     require("github-theme").setup({})
+  --   end,
+  -- },
 
   {
     "EdenEast/nightfox.nvim",
@@ -24,80 +24,20 @@ return {
     end,
   },
 
-  {
-    "navarasu/onedark.nvim",
-  },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = false,
+  --   opts = {
+  --     theme = "dragon",
+  --   },
+  --   config = function()
+  --     require("kanagawa").setup()
+  --   end,
+  -- },
 
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      contrast = "hard",
-    },
-    config = function(_, opts)
-      require("gruvbox").setup(opts)
-    end,
-  },
-
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    opts = {
-      theme = "dragon",
-    },
-    config = function()
-      require("kanagawa").setup()
-    end,
-  },
-
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        snacks = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-
-  {
-    "dasupradyumna/midnight.nvim",
-    lazy = false,
-    priority = 10,
-  },
+  -- {
+  --   "dasupradyumna/midnight.nvim",
+  --   lazy = false,
+  --   priority = 10,
+  -- },
 }
