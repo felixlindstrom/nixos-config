7e2529149b073bb231e5a37ea9ff073a92478479
Felix Lindström <felix.lindstrom@gmail.com>
2025-06-03 21:00:30 +0200

    Add rofi calc

--- hosts/_shared/home/modules/programs/rofi.nix
+++ hosts/_shared/home/modules/programs/rofi.nix
@@ -2,6 +2,9 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    plugins = with pkgs; [
+      (rofi-calc.override { rofi-unwrapped = rofi-wayland-unwrapped; })
+    ];
     theme = {
       "*" = {
         font-size = "10px";
