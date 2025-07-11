074ce573cc63acbf85c7968e5cc247724ebecb96
Felix Lindstrom <felix.lindstrom@gmail.com>
2025-07-11 20:57:45 +0200

    Add lock styling

--- hosts/_shared/home/modules/config.nix
+++ hosts/_shared/home/modules/config.nix
@@ -5,18 +5,20 @@ let
   assetsDir = ../../../../assets;
 
   mkooss = config.lib.file.mkOutOfStoreSymlink;
-  linkConfig = filePath: mkooss "${config.home.homeDirectory}/Projects/nixos-config/config/${filePath}"; # Change this to be relative..
+  linkConfig =
+    filePath: mkooss "${config.home.homeDirectory}/Projects/nixos-config/config/${filePath}"; # Change this to be relative..
 in
 {
   xdg.configFile."starship.toml".source = linkConfig "starship.toml";
   xdg.configFile."niri/config.kdl".source = linkConfig "niri/config.kdl";
+  xdg.configFile."hypr".source = linkConfig "hypr";
   home.file = {
     ".config/mako".source = "${configDir}/mako";
     ".config/rofi".source = "${configDir}/rofi";
     ".config/waybar".source = "${configDir}/waybar";
     ".config/wezterm".source = "${configDir}/wezterm";
     ".config/zsh".source = "${configDir}/zsh";
-    ".config/hypr".source = "${configDir}/hypr";
+    # ".config/hypr".source = "${configDir}/hypr";
     ".zshrc".source = "${configDir}/zsh/.zshrc";
     "bin".source = "${binDir}";
     "wallpapers".source = "${assetsDir}/wallpapers";
